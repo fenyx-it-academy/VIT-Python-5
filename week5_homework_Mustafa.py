e7d6b4bae641d2a0e365205ee7adf13910cf5104
Μustafa ÜΝΑL <52699858+muungas@users.noreply.github.com>
2023-04-07 00:48:08 +0300

    Update week5_homework_Mustafa.py

--- week5_homework_Mustafa.py
+++ week5_homework_Mustafa.py
@@ -94,3 +94,15 @@ if __name__ == '__main__':
     print(a//b)
     print(a/b)
 
+# 2 =============
+# Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores.
+# Store them in a list and find the score of the runner-up.
+# Input Format
+# The first line contains n. The second line contains an array A[] of n integers each separated by a space.
+# Constraints
+# • 2 ≤ n ≤ 10
+# •-100 A[i] < 100
+# Output Format
+# Print the runner-up score.
+# Given list is [2, 3, 6, 6, 5]. The maximum score is 6. second maximum is 5. Hence, we print 5 as the runner-up score.
+
